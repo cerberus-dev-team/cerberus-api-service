e35f676799793f7e90b8a22995bf8f80ca23314f
Daniel Quintero <quinterod164@gmail.com>
2023-09-27 18:48:15 -0500

    chore: :wrench: add config files for test, eslint, ts

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ["eslint-config-codely/typescript"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+    },
+  ],
+  rules: {
+    "no-console": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-misused-promises": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "padding-line-between-statements": "off",
+    "@typescript-eslint/unbound-method": "off",
+    "prettier/prettier": ["error", { endOfLine: "auto", semi: false }],
+    semi: ["error", "never"],
+  },
+}
